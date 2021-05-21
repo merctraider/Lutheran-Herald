373d57a5c9eec6c410ad8e075c0f56d9a2de252c
merctraider <47917658+merctraider@users.noreply.github.com>
2021-05-21 10:07:31 +0800

    Pull the full text
    Readings widget now pulls the full text without headings or verses

--- inc/class-lutherald-BibleGateway.php
+++ inc/class-lutherald-BibleGateway.php
@@ -19,14 +19,28 @@ class BibleGateway{
 
     public static function get_verse($lookup){
         $lookup = \urlencode($lookup);
-
+        
         $ver = self::$version;
         $url = "http://www.biblegateway.com/passage/?search=$lookup&version=$ver";
+        /*
         $content = self::fetch_url($url);
         if (preg_match('/<meta property="og:description" content="([^\/]+)"/', $content, $matches)){
             $verse = $matches[1];
             return $verse;
+        }*/
+        require_once  plugin_dir_path(__FILE__) .'simple_html_dom.php';
+        $content = file_get_html($url);
+        $output = '';
+        $passage_html = $content->find('div.passage-text', 0);
+        foreach($passage_html->find('p') as $verse){
+            foreach($verse->find('sup') as $footnote){
+                $footnote->innertext = '';
+            }
+
+            $output .= $verse->plaintext;
         }
+        return $output;
+        
         return false;      
         
     }
