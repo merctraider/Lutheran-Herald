71f90632f6f261bd195621ae705165b09adbc759
merctraider <47917658+merctraider@users.noreply.github.com>
2021-07-28 18:45:14 +0800

    removed line breaks in special feasts

--- lutheran-herald.php
+++ lutheran-herald.php
@@ -3,7 +3,7 @@
  * Plugin Name: Lutheran Herald
  * Description: A plugin made for ELDoNA.
  * Author: merctraider
- * Version: 1.0.0
+ * Version: 1.0.1
  * Requires PHP: 5.4
  * Author URI: http://merctraider.me
  */
